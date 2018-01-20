d57bead668ca331c002eb3afdc04c2a09cdaf7a8
Gustavo Cruz <gustavo.cruz.laura@gmail.com>
2018-01-20 18:54:01 -0400

    Episode 11: laracasts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -12,5 +13,15 @@ class PostsController extends Controller
 
     public function show() {
         return view('posts.show');
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store() {
+        Post::create(request(['title', 'body']));
+
+        return redirect('/');
     }
 }
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="col-sm-8 blog-main">
+        <h1>Create a Post</h1>
+        <form method="POST" action="/posts">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="title">Email address</label>
+                <input type="text" class="form-control" id="title" aria-describedby="title" name="title" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="body">Example textarea</label>
+                <textarea class="form-control" id="body" name="body" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+@endsection
